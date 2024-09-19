9d8e34bf299289765ea32da637f6652cad953deb
3hos15 <145386421+3hos15@users.noreply.github.com>
2024-09-19 12:21:52 +0200

    19 september

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
 // JavaScript Document
 console.log("hi");
+
+let deButton = document.querySelector("section button");
+let hamburger = document.querySelector("header section");
+let deNav = document.querySelector("header section section");
+
+deButton.onclick = toggleMenu;
+
+function toggleMenu() {
+    deNav.classList.toggle("menuOpen");
+    hamburger.classList.toggle("menuOpen")
+}
